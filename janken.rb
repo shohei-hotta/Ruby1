dee0c719ec89d56f85a61a62f375a930503e8084
shohei-hotta <sho1127-cal@kra.biglobe.ne.jp>
2020-03-11 09:21:12 +0000

    Jankenを変更

--- janken.rb
+++ janken.rb
@@ -1,24 +1,17 @@
 # プレイヤー(自分)に「0 ~ 2」を入力させるロジックを書きます。
 class Player
   def hand
-    # 「プレイヤーに 0 ~ 2を入力させる文章」を表示させます。
     puts "0~2の数字を入力してください。"
     puts "0:グー, 1:チョキ, 2:パー"
-    # 変数「input_hand」に
-    # プレイヤーが入力したものを取得して代入します。
-    # ヒント! 「getsメソッド」を使用します。
-    input_hand = gets.to_i
-    # 「input_hand」(取得した値)が「0, 1, 2」のいずれかだとwhileから脱出させ、それ以外だと初めから繰り返させます。
-    # ヒント! 「while文」を使用します。
-      # 【条件分岐】「input_hand」(取得した値)が「0, 1, 2」のいずれかの場合だったとき
-        # 「input_hand」(取得した値)をそのまま返します。
-        # ヒント！ 「return」を使用します。
+    input_hand = gets.chomp
+    if input_hand == "0" || input_hand == "1" || input_hand == "2"
+     input_hand = input_hand.to_i
+    else
+      return hand
+    end
     if input_hand == 0 || input_hand == 1 || input_hand == 2
       return input_hand
-    else # 条件外だった場合
-          # 「プレイヤーに 0 ~ 2を入力させる文章」を表示させます。
-          # 変数「input_hand」に
-          # プレイヤーが入力したものを取得して代入します。
+    else
       return hand
     end
   end
@@ -28,12 +21,11 @@ end
 class Enemy
   def hand
     # グー、チョキ、パーの値をランダムに出力させます。
-    enemy_hand = "#{rand(3)}"
+    enemy_hand = rand(3)
     return enemy_hand
   end
 end
 
-=begin
 # プレイヤー(自分)が入力した「0 ~ 2」と、敵がランダムで生成した「0 ~ 2」をじゃんけんをさせて、その結果をコンソール上に出力するロジックを書きます。
 class Janken
   def pon(player_hand, enemy_hand)
@@ -41,29 +33,26 @@ class Janken
     janken = ["グー", "チョキ", "パー"]
     #「相手の手は#{敵の生成した値}です。」と出力させます。
     puts "相手の手は#{janken[enemy_hand]}です。"
-    # 【条件分岐】もしもプレイヤーと敵の出した値が同じだった場合
-      # 「あいこ」を出力します。
-      # whileを繰り返す「true」を返してじゃんけんを繰り返し実行させます。
+    if (player_hand - enemy_hand + 3) % 3 == 0
+      puts "あいこです。"
       return true
-    # もしも下記の組み合わせだった場合
-      # 例 (player_hand == 0 && enemy_hand == 1) || (player_hand == 1 && enemy_hand == 2) || (player_hand == 2 && enemy_hand == 0)
-      # 「あなたの勝ちです」を出力します。
+      # whileを繰り返す「true」を返してじゃんけんを繰り返し実行させます。
+    elsif (player_hand - enemy_hand + 3) % 3 == 2
+      puts "あなたの勝ちです。"
+      return false
       # whileを終了させる「false」を返してじゃんけんを終了させます。
-    #  それ以外だった場合
-      # 「あなたの負けです」を出力します。
+    else
+      puts "あなたの負けです。"
+      return false
       # whileを終了させる「false」を返してじゃんけんを終了させます。
     end
   end
 end
-# 変数「player」にPlayerをインスタンス化したものを代入します。
+
 player = Player.new
-# 変数「enemy」にEnemyをインスタンス化したものを代入します。
 enemy = Enemy.new
-# 変数「janken」にJankenをインスタンス化したものを代入します。
 janken = Janken.new
-# 変数「next_game」に「true」を代入します。
 next_game = true
-# 「next_game」が「false」にならない限り、永久にwhileで繰り返し処理を実行します。
 while next_game
 # じゃんけん
 # 変数「next_game」に
@@ -71,6 +60,3 @@ while next_game
 # じゃんけんの実行結果に「true」が返ってくると、このwhileを繰り返します。
   next_game = janken.pon(player.hand, enemy.hand)
 end
-
-
-=end
